b9872afbd6ff80b1cd9ae8ee255bdb2aa1744a25
LashayC <cokleylashay@gmail.com>
2022-09-08 12:21:48 -0400

    removed comments in schema

--- models/VacationSpot.js
+++ models/VacationSpot.js
@@ -1,17 +1,13 @@
 //Creating the model for the vacation spots we want in the vacation database
-
-//Create the Schema
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-//What you want in db and how they can be represented.
 const vacationSpotSchema = new Schema({
-    destination: {type: String, unique: true}, //The 'unique' option creates a unique index. Ensures can't have 2 docs w/ same name
+    destination: {type: String, unique: true}, 
     location: String,
     description: String,
     photo: String,
 })
 
-//Create model using mongoose method 'model' and use module.exports(tells node which code to export from a file so other files can access it)
 module.exports = mongoose.model('VacationSpot', vacationSpotSchema)
 
